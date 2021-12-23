49e7665ebc3fba55c19c1efa0d83cc93b83af3fb
Glenn Marshall-Adams <glenn.marshalladams@icloud.com>
2021-12-23 21:45:15 +0000

    Inject Literal Values

--- src/com/luv2code/springdemo/CricketCoach.java
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -2,9 +2,29 @@ package com.luv2code.springdemo;
 
 public class CricketCoach implements Coach {
     private FortuneService fortuneService;
+    private String emailAddress;
+    private String team;
 
     public CricketCoach() {
         System.out.println("CricketCoach: inside no-arg constructor");
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
+        this.team = team;
     }
 
     //setter method to be called by spring
@@ -22,4 +42,5 @@ public class CricketCoach implements Coach {
     public String getDailyFortune() {
         return fortuneService.getFortune();
     }
+
 }
