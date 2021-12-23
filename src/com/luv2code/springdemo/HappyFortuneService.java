6508997c09f68502fac738181936b2cd8cf10941
Glenn Marshall-Adams <glenn.marshalladams@icloud.com>
2021-12-23 22:05:43 +0000

    Inject Literal Values from properties file & inclusion of random fortune generator

--- src/com/luv2code/springdemo/HappyFortuneService.java
+++ src/com/luv2code/springdemo/HappyFortuneService.java
@@ -1,8 +1,18 @@
 package com.luv2code.springdemo;
 
+import java.util.Random;
+
 public class HappyFortuneService implements FortuneService {
+
+    public String[] arr = {"Today is your lucky day!", "You are lucky!", "Today is your day!"};
+
+    public static String getRandom(String[] array) {
+        int rand = new Random().nextInt(array.length);
+        return array[rand];
+    }
+
     @Override
     public String getFortune() {
-        return "Today is your lucky day!";
+        return getRandom(arr);
     }
 }
