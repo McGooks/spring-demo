d3527fbb6ce933a0780bd20ae0351ebff197c9e8
Glenn Marshall-Adams <glenn.marshalladams@icloud.com>
2021-12-27 12:14:56 +0000

    Simple object comparison

--- src/com/luv2code/springdemo/beanScopeDemoApp.java
+++ src/com/luv2code/springdemo/beanScopeDemoApp.java
@@ -9,5 +9,10 @@ public class beanScopeDemoApp {
         // retrieve beans from spring container
         Coach theCoach = context.getBean("myCoach", Coach.class);
         Coach alphaCoach = context.getBean("myCoach", Coach.class);
+
+        boolean result = (theCoach == alphaCoach);
+        System.out.println("Pointing to the same object: " + result);
+        System.out.println("Memory location for theCoach: " + theCoach);
+        System.out.println("Memory location for alphaCoach: " + alphaCoach + "\n");
     }
 }
